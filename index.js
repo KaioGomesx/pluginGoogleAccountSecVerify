603582f1386c10cc6e31e4a52fe52c93be35d6b8
KaioGomesx <kaiogomes.nave@gmail.com>
2020-01-07 06:53:15 -0300

    Checking if you have an aria-label saying there are security issues

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const ariaLabel = document
+  .querySelector("#i5 > div.uKKLyf > div.BkKHtf")
+  .getAttribute("aria-label");
+
+if (ariaLabel === "Problemas encontrados no login e recuperação") {
+  alert("Há problemas de segurança em sua conta, resolva-os");
+} else if (ariaLabel === "Login and recovery issues") {
+  alert("There are security issues with your account, please fix them");
+}
 /*
 HLEawd VfPpkd-ksKsZd-XxIAqe
 */
